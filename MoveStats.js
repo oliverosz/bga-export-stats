201cd3b81ec7bb498b999282901fc586b871f787
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2020-07-31 21:52:27 +0200

    Add Remaining Time column

--- MoveStats.js
+++ MoveStats.js
@@ -10,12 +10,13 @@ javascript:{
         var gameName = titleStr[2];
         var tableID = titleStr[3];
         var output = "";
-        var logs = document.querySelector("#gamelogs").children;
+        var players = Array.from(document.querySelectorAll("#game_result .name")).map(item => item.innerText);
         var moveNo = 0;
         var date = "";
         var time = "";
+        var remaining = "";
         var player = "";
-        var players = Array.from(document.querySelectorAll("#game_result .name")).map(item => item.innerText);
+        var logs = document.querySelector("#gamelogs").children;
         for (var i = 0; i < logs.length; i++) {
             if (logs[i].className == "smalltext") {
                 moveNo = logs[i].innerText.match(/Move (\d+) ?:/)[1];
@@ -26,10 +27,19 @@ javascript:{
                 }
                 player = "";
             }
-            else if (player == "" && logs[i].className.includes("gamelogreview")) {
-                var words = logs[i].innerText.split(" ");
-                player = words.find(word => players.includes(word));
-                output = output + [tableID, gameName, moveNo, JSDateToExcelDate(new Date(date + time)), player].join(";") + "\n";
+            else if (player == "") {
+                if (logs[i].className.includes("gamelogreview")) {
+                    var words = logs[i].innerText.split(" ");
+                    player = words.find(word => players.includes(word));
+                }
+            } else if (logs[i].className.includes("reflexiontimes_block")) {
+                for (var j = 0; j < logs[i].childElementCount; j++) {
+                    if (logs[i].children[j].innerText.indexOf(player) == 0) {
+                        remaining = logs[i].children[j].children[0].innerText;
+                        output = output + [tableID, gameName, moveNo, JSDateToExcelDate(new Date(date + time)), remaining, player].join(";") + "\n";
+                        break;
+                    }
+                }
             }
         }
         /* create export box or remove it if already exists */
@@ -39,7 +49,7 @@ javascript:{
             div.setAttribute("id", "pagesection_export");
             div.className = "pagesection";
             var header = document.createElement("h3");
-            header.innerText = "Table ID;Game Name;Move No.;Date Time;Player Name"; /* column headers in the box title */
+            header.innerText = "Table ID;Game Name;Move No.;Date Time;Remaining Time;Player Name"; /* column headers in the box title */
             var exported = document.createElement("div");
             /* select all text when clicked */
             exported.setAttribute("style", "-webkit-touch-callout: all; -webkit-user-select: all; -khtml-user-select: all; -moz-user-select: all; -ms-user-select: all; user-select: all;");
